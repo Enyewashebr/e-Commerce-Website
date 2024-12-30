585dc1f8f319258a401c7092cf99d91c246638e7
Enyewashebr <Enyewashebir0@gmail.com>
2024-12-30 14:18:14 +0300

    eliminating error in newsletter box component

--- Enex-Boutique/src/components/NewsLetterBox.jsx
+++ Enex-Boutique/src/components/NewsLetterBox.jsx
@@ -10,7 +10,7 @@ const NewsLetterBox = () => {
       <p className="text-gray-400 mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus sequi pariatur nulla quisquam</p>
       <form className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
         <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your email address" required/>
-        <button type="submit" className="bg-black text-white text-xs px-10 py-4">SUBSCRIBE</button>
+        <button onClick={onSubmitHandler} type="submit" className="bg-black text-white text-xs px-10 py-4">SUBSCRIBE</button>
       </form>
     </div>
   )
