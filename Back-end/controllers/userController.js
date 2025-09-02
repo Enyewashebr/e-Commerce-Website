cfacdae5d53692af6fb1c57cfa40bdea9bf71ebc
Enyewashebr <Enyewashebir0@gmail.com>
2025-09-02 14:58:54 +0300

    Add user controller with login, registration, and admin login routes

---
+++ Back-end/controllers/userController.js
@@ -0,0 +1,13 @@
+
+
+// Rout for user login
+const loginUser = async (req, res) =>{}
+
+
+// Rout for user registration
+const registerUser = async (req, res) =>{}
+
+
+// Rout for admin login
+const adminLogin = async (req, res) =>{}
+export { loginUser, registerUser, adminLogin };
