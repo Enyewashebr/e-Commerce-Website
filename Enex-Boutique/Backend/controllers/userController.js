8bb7d086e0944527c43a8066714eb0e86ff853be
Enyewashebr <Enyewashebir0@gmail.com>
2026-03-26 12:29:37 +0300

    feat: Implement user authentication and product management features with Cloudinary integration

---
+++ Enex-Boutique/Backend/controllers/userController.js
@@ -0,0 +1,19 @@
+
+
+// route for user login
+const loginUser = async (req, res) => {
+
+}
+
+
+// route for user registration
+const registerUser = async (req, res) => {
+    
+}
+
+// route for admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
--- Enex-Boutique/Backend/server.js
+++ Enex-Boutique/Backend/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from "cors";
 import "dotenv/config"
 import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 // App config
 
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 
 // Middleware
@@ -16,7 +19,7 @@ app.use(cors())
 
 
 //api endpoints
-
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res)=>{
     res.send("API working")
